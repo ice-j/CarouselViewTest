b9bef0d90e41bdb424066d8ccfbfe5233b4f129a
Ice Jovanoski <ice.jovanoski@haselt.com>
2016-06-29 09:32:58 +0200

    Initial commit

--- CarouselViewTest/CarouselViewTest/App.cs
+++ CarouselViewTest/CarouselViewTest/App.cs
@@ -4,25 +4,17 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace CarouselViewTest
 {
 	public class App : Application
 	{
 		public App ()
 		{
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            // The root page of your application
+            MainPage = new TheMainPage();
 		}
 
 		protected override void OnStart ()
---
+++ CarouselViewTest/CarouselViewTest/TheMainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace CarouselViewTest
+{
+	public partial class TheMainPage : ContentPage
+	{
+        public ObservableCollection<GalleryImage> Images { get; set; } = new ObservableCollection<GalleryImage>();
+
+        public TheMainPage()
+		{
+			InitializeComponent ();
+            Images.Add(new GalleryImage
+            {
+                ImgUrl = "https://www.xamarin.com/content/images/pages/branding/assets/xamagon.png"
+            });
+            Images.Add(new GalleryImage
+            {
+                ImgUrl = "https://www.xamarin.com/content/images/pages/branding/assets/xamagon.png"
+            });
+            Images.Add(new GalleryImage
+            {
+                ImgUrl = "https://www.xamarin.com/content/images/pages/branding/assets/xamagon.png"
+            });
+        }
+	}
+
+    public class GalleryImage
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string ImgUrl { get; set; }
+    }
+}
